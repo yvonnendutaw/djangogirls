9bb6ece3c11e1f12ff5b9366f97eea27fea6ca61
yvonnendutaw <yvonita7@gmail.com>
2016-08-23 23:53:04 +0300

    added docmentations on models.py

--- djangogirls/blog/models.py
+++ djangogirls/blog/models.py
@@ -3,15 +3,18 @@ from __future__ import unicode_literals
 from django.db import models
 
 from django.utils import timezone
-# Create your models here.
 
+#indicates that we are defining the model
+#post is the name of the model
 class Post(models.Model):
+#author, title,text,createde_at, published_date are properties of the object
     author = models.ForeignKey("auth.User")
     title = models.CharField(max_length=150)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateField(blank = True, null = True)
 
+    #this is the publish method 
     def publish(self):
         self.published_date =  timezone.now()
         self.save()
