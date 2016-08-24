5a99801d0556a8100d63aa7425506ee783614374
yvonita7@gmail.com <yvonita7@gmail.com>
2016-08-24 04:06:02 +0300

    added templating

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 #where the logic of the application
+def post_list(request):
+    """function that takes request and returns a function render
+    that will render the template blog/post_list.html 
+    """
+    return render(request, 'blog/post_list.html', {})
